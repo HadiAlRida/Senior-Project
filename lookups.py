3b3778cd7b1ef05ba690cdd0e68d920326e6a575
HadiAlRida <hadirida444@gmail.com>
2024-06-09 18:09:15 +0300

    added database handler

---
+++ lookups.py
@@ -0,0 +1,19 @@
+from enum import Enum
+
+
+class ErrorHandling(Enum):
+    DB_CONNECT_ERROR = "DB Connect Error"
+    DB_RETURN_QUERY_ERROR = "DB Return Query Error"
+    API_ERROR = "Error calling API"
+    RETURN_DATA_CSV_ERROR = "Error returning CSV"
+    RETURN_DATA_EXCEL_ERROR = "Error returning Excel"
+    RETURN_DATA_SQL_ERROR = "Error returning SQL"
+    RETURN_DATA_UNDEFINED_ERROR = "Cannot find File type"
+    EXECUTE_QUERY_ERROR = "Error executing the query"
+
+
+class InputTypes(Enum):
+    SQL = "SQL"
+    CSV = "CSV"
+    EXCEL = "Excel"
+    
